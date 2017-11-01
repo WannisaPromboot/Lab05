2f45865fe21769e32b795a5bc9599621a0db2ab7
Wannamiu <58030218@kmitl.ac.th>
2017-11-01 23:04:36 +0700

    commited by wannisa

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -4,23 +4,22 @@ class Program
 {
     static void Main()
     {
-        double d = 5e9d;
-        Console.WriteLine(d);
-        int i = (int)d;
-        Console.WriteLine(i);
+        try
+        {
+            double d = 5e9d;
+            Console.WriteLine(d);
+            int i = checked((int)d);
+            Console.WriteLine(i);
 
-       
-            double myDouble = d;
-          //  Console.WriteLine(myDouble);
 
-            int myLong = (int)myDouble;
-        //  Console.WriteLine(myLong);
+        }
 
-        Console.WriteLine("Checked : double is {0} covert to {1}",d, myLong);
+        catch (Exception)
+        {
 
+
+        }
 
     }
-
-
-}
+}  
 
